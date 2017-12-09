8c6ddaaeae22abb2e8381a6c61af968909d4f25b
Jelle <vandijkjelle@gmail.com>
2017-12-10 00:06:16 +0100

    update of fisParser

--- fisParser.py
+++ fisParser.py
@@ -7,6 +7,7 @@ import re
 
 
 def parseMF(line):
+    """ Returns a membership function based on the given fis line """
     # l[1] = name
     # l[2] = mf type
     # l[3] = mf args
@@ -15,19 +16,17 @@ def parseMF(line):
     line = line.split('=')
     args = line[3].strip().replace('[', '').replace(']', '').split(' ')
 
-    # print(line[2])
     if line[2] == 'trimf':
-        print(line[1], args[0], args[1], args[2])
         mf = TriangularMF(line[1], int(args[0]), int(args[1]), int(args[2]))
 
     if line[2] == 'tramf':
-        print(line[1], args[0], args[1], args[2], args[3])
         mf = TrapezoidalMF(line[1], int(args[0]), int(args[1]), int(args[2]), int(args[3]))
 
     return mf
 
 
 def parseVar(block):
+    """Returns the variable declared in the block"""
     mfPattern = r'MF(.)*'
     namePattern = r'Name=(.)*'
     rangePattern = r'Range=(.)*'
@@ -36,24 +35,40 @@ def parseVar(block):
     name = [l for l in lines if re.match(namePattern, l)][0]
     name = name.replace("'", "").split("=")[1]
 
-    print(name)
-
     r = [l for l in lines if re.match(rangePattern, l)][0]
     r = r.replace("[", '').replace("]", '').split('=')[1].split(' ')
 
     mfs = []
-    # print([line for line in lines if re.match(mfPattern, line)])
     for l in [line for line in lines if re.match(mfPattern, line)]:
         mfs.append(parseMF(l))
 
-    print()
+    if lines[0][1] == 'I':
+        return Input(name, (int(r[0]), int(r[1])), mfs)
+    else:
+        return Output(name, (int(r[0]), int(r[1])), mfs)
 
-    return Input(name, (int(r[0]), int(r[1])), mfs)
+
+def parseInOutput(f):
+    """
+    Returns a list of in and output Variables
+    The variables are created from the given fis string
+    """
+    inPat = r'\[Input(.)*\]'
+    outPat = r'\[Output(.)*\]'
+    f = f.split('\n\n')
+
+    inputs = []
+    for block in [b for b in f if re.match(inPat, b)]:
+        inputs.append(parseVar(block))
+
+    outputs = []
+    for block in [b for b in f if re.match(outPat, b)]:
+        outputs.append(parseVar(block))
+
+    return inputs, outputs
 
 
 if __name__ == "__main__":
-    inPat = r'\[Input(.)*\]'
-    outPat = r'\[Output(.)*\]'
     if (len(sys.argv) != 2):
         print("Give fis file as argument")
         sys.exit(0)
@@ -67,16 +82,8 @@ if __name__ == "__main__":
         sys.exit(0)
 
     f = open(filename).read()
-    f = f.split('\n\n')
 
-    inputs = []
-    for block in [b for b in f if re.match(inPat, b)]:
-        inputs.append(parseVar(block))
-
-    outputs = []
-    for block in [b for b in f if re.match(outPat, b)]:
-        inputs.append(parseVar(block))
-
+    inputs, outputs = parseInOutput(f)
 
     rules = [
         Rule({"income":"low", "quality":"amazing"}, ("and", "min"), {"money":"low"}),
